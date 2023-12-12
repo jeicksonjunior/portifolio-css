f6dda3cff8d39b916b4dafd1c46ac982f20bc61d
jeicksonjunior <jeickson_lima@hotmail.com>
2023-12-12 00:52:20 -0300

    12/12/2023

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,18 @@
-const tabs = document.querySelectorAll(".tab-btn");
-const allContent = document.querySelectorAll(".tab-content");
+document.addEventListener('DOMContentLoaded', function () {
+    const tabs = document.querySelectorAll('.tab-btn');
+    const contents = document.querySelectorAll('.tab-content');
+    const line = document.querySelector('.line');
 
-tabs.forEach((tab, index) => {
-    tab.addEventListener("click", () => {
-        tabs.forEach(tab => { tab.classList.remove("active") });
-        tab.classList.add("active");
+    tabs.forEach((tab, index) => {
+        tab.addEventListener('click', () => {
+            tabs.forEach(tab => tab.classList.remove('active'));
+            tab.classList.add('active');
 
+            contents.forEach(content => content.classList.remove('active'));
+            contents[index].classList.add('active');
 
-        allContent[index].classList.add("active");
-
-    })
-
-})
+            line.style.width = `${tab.offsetWidth}px`;
+            line.style.transform = `translateX(${tab.offsetLeft}px)`;
+        });
+    });
+});
